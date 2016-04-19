a56273e90f562db0bc9d755be1034e0b62af60d6
Olivier Monnier <omonnier.dev@gmail.com>
2016-04-19 15:01:53 +0200

    fix method

--- src/originjs.js
+++ src/originjs.js
@@ -30,6 +30,7 @@ function getSource () {
 function getMedium () {
   if (document.referrer.search('https?://(.*)(google|bing|yahoo|daum|eniro|naver|msn|aol|lycos|ask|altavista|netscape|cnn|about|mamma|alltheweb|voila|virgilio|baidu|alice|yandex|najdi|mamma|seznam|search|wp|onetcenter|szukacz|yam|pchome|kvasir|ozu|terra|mynet|ekolay|rambler).([^/?]*)') === 0) {
     return 'organic';
+  } else if (document.referrer.search('https?://(.*)(facebook|twitter|linkedin).([^/?]*)') === 0 || ) {
     return 'social';
   } else {
     return 'referal';
