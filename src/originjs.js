7d139490fae1ad365f7a9033c4baf9e60ce2671c
Olivier Monnier <omonnier.dev@gmail.com>
2016-10-21 17:43:25 +0200

    add navigator method

--- src/originjs.js
+++ src/originjs.js
@@ -1,12 +1,40 @@
+function getBrowserLang () {
+  return window.navigator.lang;
+};
+
+function getMedium () {
+  if (document.referrer.search('https?://(.*)(google|bing|yahoo|daum|eniro|naver|msn|aol|lycos|ask|altavista|netscape|cnn|about|mamma|alltheweb|voila|virgilio|baidu|alice|yandex|najdi|mamma|seznam|search|wp|onetcenter|szukacz|yam|pchome|kvasir|ozu|terra|mynet|ekolay|rambler).([^/?]*)') === 0) {
+    return 'organic';
+  } else if (document.referrer.search('https?://(.*)(facebook|twitter|linkedin).([^/?]*)') === 0) {
+    return 'social';
+  } else {
+    return 'referal';
+  }
+}
+
+function getNavigator () {
+  var sBrowser, sUsrAg = navigator.userAgent;
+
+  if(sUsrAg.indexOf("Chrome") > -1) {
+    sBrowser = "Google Chrome";
+  } else if (sUsrAg.indexOf("Safari") > -1) {
+      sBrowser = "Apple Safari";
+  } else if (sUsrAg.indexOf("Opera") > -1) {
+      sBrowser = "Opera";
+  } else if (sUsrAg.indexOf("Firefox") > -1) {
+      sBrowser = "Mozilla Firefox";
+  } else if (sUsrAg.indexOf("MSIE") > -1) {
+      sBrowser = "Microsoft Internet Explorer";
+  }
+
+  return sBrowser;
+}
+
 function getParams () {
   const decompose = decodeURIComponent(params).slice(1).split('=').join('":"').split('&').join('","');
   const params = window.location.search;
 
   return (params.length > 0) ? JSON.parse(`{"${decompose}"}`) : {};
-};
-
-function getBrowserLang () {
-  return window.navigator.lang;
 };
 
 function getSource () {
@@ -27,16 +55,6 @@ function getSource () {
   }
 };
 
-function getMedium () {
-  if (document.referrer.search('https?://(.*)(google|bing|yahoo|daum|eniro|naver|msn|aol|lycos|ask|altavista|netscape|cnn|about|mamma|alltheweb|voila|virgilio|baidu|alice|yandex|najdi|mamma|seznam|search|wp|onetcenter|szukacz|yam|pchome|kvasir|ozu|terra|mynet|ekolay|rambler).([^/?]*)') === 0) {
-    return 'organic';
-  } else if (document.referrer.search('https?://(.*)(facebook|twitter|linkedin).([^/?]*)') === 0) {
-    return 'social';
-  } else {
-    return 'referal';
-  }
-}
-
 function getWindowSize () {
   return {
     height: window.innerHeight,
@@ -47,6 +65,7 @@ function getWindowSize () {
 module.exports = {
   getBrowserLang,
   getMedium,
+  getNavigator,
   getParams,
   getSource,
   getWindowSize
